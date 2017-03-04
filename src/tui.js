7c2fde31c0458af603465744d63fda9468751d58
Florent Georges <fgeorges@fgeorges.org>
2017-03-04 13:38:10 +0100

    Refactor the commands in the program module.
    Refactor the comamnds so they are all configured the same way, even if
    they are implemented differently (e.g. whether or not they accpt/rely
    on a project/environment to be passed).

--- src/tui.js
+++ src/tui.js
@@ -9,28 +9,103 @@ const cmd     = require('./commands');
 const node    = require('./node');
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * The command action implementations.
+ */
+
+// implementation of the action for any command accepting a project/environment
+function execWithProject(program, cmd)
+{
+    // the platform
+    var dry      = program.dry     ? true : false;
+    var verbose  = program.verbose ? true : false;
+    var platform = new node.Node(dry, verbose);
+    // the options
+    var env      = program.environ;
+    var path     = program.file;
+    var params   = program.param;
+    var force    = {};
+    [ 'code', 'host', 'srcdir', 'user' ].forEach(name => force[name] = program[name]);
+    if ( program.password ) {
+	force.password = read.question('Password: ', { hideEchoBack: true });
+    }
+    // the project
+    platform.project(env, path, params, force, project => {
+	// execute the command
+	project.execute(cmd.clazz);
+    });
+}
+
+// implementation of the action for command `new`
+function execNew(program, cmd)
+{
+    // check forbidden options
+    [ 'dry', 'environ', 'file', 'code', 'host', 'srcdir', 'user', 'password' ].forEach(name => {
+	if ( program[name] ) {
+	    throw new Error('Option `--' + name + '` not supported for command `' + cmd.command + '`');
+	}
+    });
+
+    // the platform
+    var verbose  = program.verbose ? true : false;
+    var platform = new node.Node(false, verbose);
+    var dir      = platform.cwd();
+
+    // Check the directory is empty...!
+    if ( fs.readdirSync(dir).length ) {
+	throw new Error('Directory is not empty: ' + dir);
+    }
+
+    // gather info by asking the user...
+    platform.log('--- ' + platform.bold('Questions') + ' ---');
+    var abbrev   = read.question('Project code    : ');
+    var title    = read.question('Title           : ');
+    var dfltName = 'http://mlproj.org/example/' + abbrev;
+    var name     = read.question('Name URI (' + dfltName + '): ', { defaultInput: dfltName });
+    var version  = read.question('Version  (0.1.0): ', { defaultInput: '0.1.0' });
+    var port     = read.question('Port     (8080) : ', { defaultInput: '8080' });
+
+    // execute the command
+    var command  = new cmd.clazz(platform, dir, abbrev, title, name, version, port);
+    var xpdir    = command.execute();
+
+    // summary
+    platform.log('\n--- ' + platform.bold('Summary') + ' ---');
+    platform.log(platform.green('✓') + ' Project created: \t' + abbrev);
+    platform.log(platform.green('→') + ' Check/edit files in: \t' + xpdir);
+}
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  * The program itself, using `commander`
  */
 
-var resolved = false;
-// command `new` is handled differently
+// the commands
 var commands = [{
+    clazz       : cmd.NewCommand,
+    command     : 'new',
+    description : 'create a new project in an empty dir',
+    impl        : execNew
+}, {
     clazz       : cmd.ShowCommand,
     command     : 'show',
-    description : 'display the environment'
+    description : 'display the environment',
+    impl        : execWithProject
 }, {
     clazz       : cmd.SetupCommand,
     command     : 'setup',
     description : 'setup the environment on MarkLogic',
     options     : [
 	// { option: '-d, --dry', label: 'dry run (do not execute, just display)' }
-    ]
+    ],
+    impl        : execWithProject
 }, {
     clazz       : cmd.DeployCommand,
     command     : 'deploy',
-    description : 'deploy modules to the modules database'
+    description : 'deploy modules to the modules database',
+    impl        : execWithProject
 }];
-var collect = (item, memo) => {
+
+// collect params from several `--param name:value` options
+var params = (item, memo) => {
     var idx = item.indexOf(':');
     if ( idx < 0 ) {
 	idx = item.indexOf('=');
@@ -44,6 +119,7 @@ var collect = (item, memo) => {
     return memo;
 };
 
+// the global options
 program
     .version('0.14.0')
     .option('-c, --code <code>',        'set/override the @code')
@@ -51,74 +127,16 @@ program
     .option('-e, --environ <name>',     'environment name')
     .option('-f, --file <file>',        'environment file')
     .option('-h, --host <host>',        'set/override the @host')
-    .option('-p, --param <name:value>', 'set/override a parameter value (use : or =)', collect, {})
+    .option('-p, --param <name:value>', 'set/override a parameter value (use : or =)', params, {})
     .option('-s, --srcdir <dir>',       'set/override the @srcdir')
     .option('-u, --user <user>',        'set/override the @user')
     .option('-v, --verbose',            'verbose mode')
     .option('-z, --password',           'ask for password interactively');
 
-// Command `new` is handled differently, as it does not rely on an environment.
-// Also, all the info gathering is dependent on the platform, web-based would
-// provide them straight to the command as the content of a form, so it is done
-// here also.
-program
-    .command('new')
-    .description('create a new project in an empty dir')
-    .action(() => {
-        resolved = true;
-	if ( program.dry ) {
-	    throw new Error('Dry run option not supported for `new`');
-	}
-	if ( program.environ ) {
-	    throw new Error('Environment name option not supported for `new`');
-	}
-	if ( program.file ) {
-	    throw new Error('Environment file option not supported for `new`');
-	}
-	if ( program.code ) {
-	    throw new Error('Code option not supported for `new`');
-	}
-	if ( program.host ) {
-	    throw new Error('Host option not supported for `new`');
-	}
-	if ( program.srcdir ) {
-	    throw new Error('Source dir option not supported for `new`');
-	}
-	if ( program.user ) {
-	    throw new Error('User option not supported for `new`');
-	}
-	if ( program.password ) {
-	    throw new Error('Password option not supported for `new`');
-	}
-	var verbose  = program.verbose ? true : false;
-	var platform = new node.Node(false, verbose);
-	var dir      = platform.cwd();
+// marker to validate a command has been resolved
+var resolved = false;
 
-	// Check the directory is empty...!
-	if ( fs.readdirSync(dir).length ) {
-	    throw new Error('Directory is not empty: ' + dir);
-	}
-
-	// gather info by asking the user...
-	platform.log('--- ' + platform.bold('Questions') + ' ---');
-	var abbrev   = read.question('Project code    : ');
-	var title    = read.question('Title           : ');
-	var dfltName = 'http://mlproj.org/example/' + abbrev;
-	var name     = read.question('Name URI (' + dfltName + '): ', { defaultInput: dfltName });
-	var version  = read.question('Version  (0.1.0): ', { defaultInput: '0.1.0' });
-	var port     = read.question('Port     (8080) : ', { defaultInput: '8080' });
-
-	// execute the command
-	var command  = new cmd.NewCommand(platform, dir, abbrev, title, name, version, port);
-	var xpdir    = command.execute();
-
-	// summary
-	platform.log('\n--- ' + platform.bold('Summary') + ' ---');
-	platform.log(platform.green('✓') + ' Project created: \t' + abbrev);
-	platform.log(platform.green('→') + ' Check/edit files in: \t' + xpdir);
-    });
-
-// Other commands are all treated the same way.
+// setup the commands
 commands.forEach(cmd => {
     var prg = program
         .command(cmd.command)
@@ -129,28 +147,15 @@ commands.forEach(cmd => {
     }
     prg.action(() => {
         resolved = true;
-	// the platform
-	var dry      = program.dry     ? true : false;
-	var verbose  = program.verbose ? true : false;
-	var platform = new node.Node(dry, verbose);
-	// the project
-	var env      = program.environ;
-	var path     = program.file;
-	var params   = program.param;
-	var force    = {};
-	[ 'code', 'host', 'srcdir', 'user' ].forEach(name => force[name] = program[name]);
-	if ( program.password ) {
-	    force.password = read.question('Password: ', { hideEchoBack: true });
-	}
-	platform.project(env, path, params, force, project => {
-	    // execute the command
-	    project.execute(cmd.clazz);
-	});
+	cmd.impl(program, cmd);
     });
 });
 
+// parse the command line and execute the command
 program.parse(process.argv);
 
+// has any command been resolved?
 if ( ! resolved ) {
+    // TODO: Add an error message ("no command or not correct, usage: ...")
     program.help();
 }
